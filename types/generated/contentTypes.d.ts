37b66e88cfbea1938223ba469edec80a69343504
z4val <danielzavaletamego@gmail.com>
2025-11-25 15:15:33 -0500

    add has_trial boolean into plan schema

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -820,6 +820,7 @@ export interface ApiPlanPlan extends Struct.CollectionTypeSchema {
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
     features: Schema.Attribute.JSON;
+    has_trial: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::plan.plan'> &
       Schema.Attribute.Private;
